2a2f8a6897ea13dd518ef2060cbd64b055aba028
Gideon6402 <g.j.wiersma@student.rug.nl>
2024-05-08 18:45:25 +0200

    managed to plot the data

--- python/main.py
+++ python/main.py
@@ -5,18 +5,28 @@ import pandas as pd
 from matplotlib.pyplot import figure, show
 
 def main():
+    fig = figure()
+    
     count = 0
     data = {}
+    measurementIndex = 1
     for measurementNumber in [1, 2, 3, 5, 6, 9, 11]:
         with open("../processed-data/" + str(measurementNumber) + ".txt", "r") as file:
             columnName = file.readline()
-            values = np.zeros((3200, 20))
-            index = 0
+            values = np.zeros(4096)
+            valueIndex = 0
             for line in file:
                 if "END OF VALUES" in line:
                     break
-                values[index] = float(line[:-1]) # debug maybe we can remove this one
-                index += 1
+                values[valueIndex] = float(line[:-1]) # debug maybe we can remove this one
+                valueIndex += 1
+            frame = fig.add_subplot(7, 1, measurementIndex)
+            frame.plot(values)
+        measurementIndex += 1
+    
+
+    show(block=False)
+
 
 if __name__ == "__main__":
     main()
