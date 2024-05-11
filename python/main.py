e2503f3e4aef624a8b66a5317c527b90f5941600
Gideon6402 <g.j.wiersma@student.rug.nl>
2024-05-11 14:21:21 +0200

    removed old python repo

--- python/main.py
+++ python/main.py
@@ -1,31 +1,14 @@
 #!/bin/python3
 
-import numpy as np
-import pandas as pd
-from matplotlib.pyplot import figure, show
+from package import *
 
 def main():
-    fig = figure()
+    data = acquire_data()
     
-    count = 0
-    data = {}
-    measurementIndex = 1
-    for measurementNumber in [1, 2, 3, 5, 6, 9, 11]:
-        with open("../processed-data/" + str(measurementNumber) + ".txt", "r") as file:
-            columnName = file.readline()
-            values = np.zeros(4096)
-            valueIndex = 0
-            for index, line in enumerate(file):
-                if "END OF VALUES" in line:
-                    break
-                values[index] = float(line[:-1]) # debug maybe we can remove this one
-            frame = fig.add_subplot(7, 1, measurementIndex)
-            frame.plot(values)
-        measurementIndex += 1
+    plot(data)
     
-
-    show()
-
+    
+        
 
 if __name__ == "__main__":
     main()
--- python/test.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python3
-
-import matplotlib.pyplot as plt
-
-for measurementNumber in [1, 2, 3, 5, 6, 9, 11]:
-    with open("../processed-data/" + str(measurementNumber) + ".txt", "r") as file:
-        for line in file:
-            if "END OF VALUES" in line:
-                column = 
