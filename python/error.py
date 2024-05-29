3a7e57d7950884eed7347af914a50a45780a5a43
Gideon Wiersma <g.j.wiersma@student.rug.nl>
2024-05-29 21:52:23 +0200

    started creating errors

--- python/error.py
+++ python/error.py
@@ -7,8 +7,12 @@ def main():
     newData = separate_runs(data)
 
     intensities = get_intensities(newData[3]["onlySodiumLamp"])
+    standardDevIntensities = np.std(intensities)
 
-    plt.scatter(range(len(intensities)), intensities)
+    timeArray = 4 * np.arange(len(intensities))
+
+    plt.errorbar(timeArray, intensities, standardDevIntensities, .5)
+
     plt.show()
 
     # get_intensity_plot(newData[3]["onlySodiumLamp"], "sodium lamp")
